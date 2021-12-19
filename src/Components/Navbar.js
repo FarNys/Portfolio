de0a3648c275a24639566ab377dd61980269abfa
FarNys <faridsalmaniy@gmail.com>
2021-12-19 08:07:55 -0700

    Repo Initial

---
+++ src/Components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar_container">
+      <ul>
+        <li>
+          <a href="">Main</a>
+        </li>
+        <li>
+          <a href="">Project</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
