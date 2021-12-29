b5af80b3f2111daccc73a2b3d26af852e7d3ffd2
FarNys <faridsalmaniy@gmail.com>
2021-12-29 09:03:05 -0700

    Change About

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,7 +12,7 @@ const About = () => {
           HTML/CSS and immediately find my path to become a developer.
           simplicity capability of styling everything was the main reason. Next
           step for sure was JavaScript as the one and only frontend web language
-          and then the most famous framework React.
+          and then master the most famous framework React.
           <br />
           I started to make tons of projects that you can find some projects in
           top part.
@@ -20,8 +20,10 @@ const About = () => {
           Nowadays i enjoy creating website and webapp with modern technologies
           and stacks like <span>React</span>, <span>Redux</span>,{" "}
           <span>Sass</span>, <span>Material UI</span>, <span>Github</span> and
+          ... .
+          {/* and
           ... , Next step is mastering JavaScript backend technology like
-          <span> NodeJs</span>, <span>Express</span> and <span>MongoDB</span>.{" "}
+          <span> NodeJs</span>, <span>Express</span> and <span>MongoDB</span>.{" "} */}
         </p>
         <div className="shape_one"></div>
         <div className="shape_two"></div>
